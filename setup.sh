a95677df954614d07666f4d5e6ce16db318fa28c
slm960323 <simengl2@illinois.edu>
2019-11-14 01:56:02 +0000

    add flags

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ cd cmake-3.*
 make -j$(nproc)
 sudo make install
 cd ../
-sudo yum install openmpi-devel
-sudo yum install gcc kernel-devel make ncurses-devel libevent
+sudo yum install openmpi-devel --skip-broken
+sudo yum install gcc kernel-devel make ncurses-devel libevent libevent-devel -skip-broken
 curl -LOk https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
 tar -xf tmux-2.8.tar.gz
 cd tmux-2.8
@@ -15,3 +15,8 @@ make
 sudo make install
 
 tmux -V
+
+cd ~
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
