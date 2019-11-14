9899763a0b1f591a44acba2b16e5833a02d557fa
slm960323 <simengl2@illinois.edu>
2019-11-14 00:49:11 +0000

    cloud setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
+tar zxvf cmake-3.*
+cd cmake-3.*
+./bootstrap --prefix=/usr/local
+make -j$(nproc)
+sudo make install
+cd ../
+sudo yum install openmpi-devel
+sudo yum install gcc kernel-devel make ncurses-devel libevent
+curl -LOk https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
+tar -xf tmux-2.8.tar.gz
+cd tmux-2.8
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make
+sudo make install
+
+tmux -V
