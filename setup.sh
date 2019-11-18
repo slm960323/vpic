6d88f212d112dfe815b57175ee7c9cdbdbb47e9e
slm960323 <simengl2@illinois.edu>
2019-11-18 05:54:21 +0000

    avx2 flags

--- setup.sh
+++ setup.sh
@@ -7,16 +7,3 @@ sudo make install
 cd ../
 sudo yum install openmpi-devel --skip-broken
 sudo yum install gcc kernel-devel make ncurses-devel libevent libevent-devel -skip-broken
-curl -LOk https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz
-tar -xf tmux-2.8.tar.gz
-cd tmux-2.8
-LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
-make
-sudo make install
-
-tmux -V
-
-cd ~
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
